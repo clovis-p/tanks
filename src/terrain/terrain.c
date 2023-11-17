73b8a936897147061d96dc0f3cec2057c73f56cd
Clovis <clovisperreault@gmail.com>
2023-11-17 00:31:15 -0500

    Round craters

--- src/terrain/terrain.c
+++ src/terrain/terrain.c
@@ -90,6 +90,7 @@ void refreshTerrain(SDL_Renderer* ren, terrain_s* terrain)
 // Function to draw a circle in a 2D array
 void drawCrater(terrain_s* terrain, int x, int y, int radius)
 {
+    /*
     for (int i = 0; i < RESOLUTION_X; i++)
     {
         for (int j = 0; j < RESOLUTION_Y; j++)
@@ -102,15 +103,15 @@ void drawCrater(terrain_s* terrain, int x, int y, int radius)
     }
 
     terrain->refreshNeeded = 1;
+*/
 
-    /*
-    // Check if the center and radius are within bounds
-    if (x > 0 && x <= RESOLUTION_X && y > 0 && y <= RESOLUTION_Y && radius > 0)
+// Check if the center and radius are within bounds
+    if (x < 0 || x >= RESOLUTION_X || y < 0 || y >= RESOLUTION_Y || radius < 0)
     {
-        printf("Invalid parameters.\n");
+        printf("drawFilledCrater(): Invalid parameters (%d, %d, %d)\n", x, y, radius);
         return;
     }
-
+/*
     // Midpoint circle drawing algorithm
     int centerX = x;
     int centerY = y;
@@ -122,23 +123,16 @@ void drawCrater(terrain_s* terrain, int x, int y, int radius)
     while (xCoord <= yCoord)
     {
         // Draw the circle points in the array
-        if (centerX + xCoord >= 0 && centerX + xCoord < RESOLUTION_X && centerY + yCoord >= 0 && centerY + yCoord < RESOLUTION_Y)
-            terrain->terrainArray[centerY + yCoord][centerX + xCoord] = 0;
-        if (centerX - xCoord >= 0 && centerX - xCoord < RESOLUTION_X && centerY + yCoord >= 0 && centerY + yCoord < RESOLUTION_Y)
-            terrain->terrainArray[centerY + yCoord][centerX - xCoord] = 0;
-        if (centerX + xCoord >= 0 && centerX + xCoord < RESOLUTION_X && centerY - yCoord >= 0 && centerY - yCoord < RESOLUTION_Y)
-            terrain->terrainArray[centerY - yCoord][centerX + xCoord] = 0;
-        if (centerX - xCoord >= 0 && centerX - xCoord < RESOLUTION_X && centerY - yCoord >= 0 && centerY - yCoord < RESOLUTION_Y)
-            terrain->terrainArray[centerY - yCoord][centerX - xCoord] = 0;
-
-        if (centerX + yCoord >= 0 && centerX + yCoord < RESOLUTION_Y && centerY + xCoord >= 0 && centerY + xCoord < RESOLUTION_X)
-            terrain->terrainArray[centerY + xCoord][centerX + yCoord] = 0;
-        if (centerX - yCoord >= 0 && centerX - yCoord < RESOLUTION_Y && centerY + xCoord >= 0 && centerY + xCoord < RESOLUTION_X)
-            terrain->terrainArray[centerY + xCoord][centerX - yCoord] = 0;
-        if (centerX + yCoord >= 0 && centerX + yCoord < RESOLUTION_Y && centerY - xCoord >= 0 && centerY - xCoord < RESOLUTION_X)
-            terrain->terrainArray[centerY - xCoord][centerX + yCoord] = 0;
-        if (centerX - yCoord >= 0 && centerX - yCoord < RESOLUTION_Y && centerY - xCoord >= 0 && centerY - xCoord < RESOLUTION_X)
-            terrain->terrainArray[centerY - xCoord][centerX - yCoord] = 0;
+        for (int i = centerX - yCoord; i <= centerX + yCoord; i++)
+        {
+            for (int j = centerY - xCoord; j <= centerY + xCoord; j++)
+            {
+                if (i >= 0 && i < RESOLUTION_X && j >= 0 && j < RESOLUTION_Y)
+                {
+                    terrain->terrainArray[i][j] = 0;
+                }
+            }
+        }
 
         // Update coordinates and decision parameter
         xCoord++;
@@ -152,8 +146,23 @@ void drawCrater(terrain_s* terrain, int x, int y, int radius)
             d = d + 4 * xCoord + 6;
         }
     }
+*/
+    // Iterate over the circle region and set the corresponding elements in the array to 0
+    for (int i = x - radius; i <= x + radius; i++)
+    {
+        for (int j = y - radius; j <= y + radius; j++)
+        {
+            // Check if the current point is within the circle using the circle equation
+            if ((i - x) * (i - x) + (j - y) * (j - y) <= radius * radius)
+            {
+                // Check if the current point is within the bounds of the array
+                if (i >= 0 && i < RESOLUTION_X && j >= 0 && j < RESOLUTION_Y)
+                {
+                    terrain->terrainArray[i][j] = 0;
+                }
+            }
+        }
+    }
 
     terrain->refreshNeeded = 1;
-
-     */
 }
