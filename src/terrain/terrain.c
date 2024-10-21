c3c474fa8f1fc5535bcb8d3e50b61e2c184977ca
clovis <clovis-p@proton.me>
2024-10-21 12:20:05 -0400

    Remove commented code

--- src/terrain/terrain.c
+++ src/terrain/terrain.c
@@ -87,67 +87,17 @@ void refreshTerrain(SDL_Renderer* ren, terrain_s* terrain)
     terrain->refreshNeeded = 0;
 }
 
-
 // Function to draw a circle in a 2D array
 void drawCrater(terrain_s* terrain, int x, int y, int radius)
 {
-    /*
-    for (int i = 0; i < RESOLUTION_X; i++)
-    {
-        for (int j = 0; j < RESOLUTION_Y; j++)
-        {
-            if (i > x && i < x + radius * 2 && j > y && j < y + radius * 2)
-            {
-                terrain->terrainArray[i][j] = 0;
-            }
-        }
-    }
-
-    terrain->refreshNeeded = 1;
-*/
-
-// Check if the center and radius are within bounds
+    // Check if the center and radius are within bounds
     if (x < 0 || x >= RESOLUTION_X || y < 0 || y >= RESOLUTION_Y || radius < 0)
     {
         printf("drawFilledCrater(): Invalid parameters (%d, %d, %d)\n", x, y, radius);
         return;
     }
-/*
-    // Midpoint circle drawing algorithm
-    int centerX = x;
-    int centerY = y;
-    int r = radius;
-    int xCoord = 0;
-    int yCoord = r;
-    int d = 3 - 2 * r;
 
-    while (xCoord <= yCoord)
-    {
-        // Draw the circle points in the array
-        for (int i = centerX - yCoord; i <= centerX + yCoord; i++)
-        {
-            for (int j = centerY - xCoord; j <= centerY + xCoord; j++)
-            {
-                if (i >= 0 && i < RESOLUTION_X && j >= 0 && j < RESOLUTION_Y)
-                {
-                    terrain->terrainArray[i][j] = 0;
-                }
-            }
-        }
-
-        // Update coordinates and decision parameter
-        xCoord++;
-        if (d > 0)
-        {
-            yCoord--;
-            d = d + 4 * (xCoord - yCoord) + 10;
-        }
-        else
-        {
-            d = d + 4 * xCoord + 6;
-        }
-    }
-*/
+    // todo: itérer autour de la zone d'impact au lieu d'itérer l'écran au complet
     // Iterate over the circle region and set the corresponding elements in the array to 0
     for (int i = x - radius; i <= x + radius; i++)
     {
